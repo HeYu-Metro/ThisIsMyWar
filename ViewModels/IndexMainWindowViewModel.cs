2b8efd8d9dafa066d6658c5de72354ebe394e0b5
hexiancheng123 <64899191+HeYu-Metro@users.noreply.github.com>
2023-06-06 17:52:08 +0800

    连接阿里云数据库

--- ViewModels/IndexMainWindowViewModel.cs
+++ ViewModels/IndexMainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Prism.Mvvm;
+﻿using MySql.Data.MySqlClient;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ThisIsMyWar.ViewModels
 {
@@ -11,11 +14,38 @@ namespace ThisIsMyWar.ViewModels
 
     internal class IndexMainWindowViewModel : BindableBase
     {
-        private string title="你好，世界！";
+        private string title;
         public string Title
         {
             get { return title; }
             set { SetProperty(ref title, value); }
+        }
+        string connectionString = "server=rm-cn-pe3390ehc0013txo.rwlb.rds.aliyuncs.com;user id=root;password=ZAQ12wsx;persistsecurityinfo=True;database=ThisIsMyWar;charset=utf8;";
+        public IndexMainWindowViewModel()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM User WHERE ID=2;";
+                    string name="";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            name = reader.GetString("Name");
+                        }
+                    }
+                    Title = name;
+                }
+                catch (Exception ex)
+                {
+                    // 处理连接错误
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
