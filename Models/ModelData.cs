5cc64e41bea3e8de0d1ca9a04b01e3b8e4f34d90
hexiancheng123 <64899191+HeYu-Metro@users.noreply.github.com>
2023-06-07 16:54:44 +0800

    完善数据库连接与数据库操作

---
+++ Models/ModelData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsMyWar.Models
+{
+    /// <summary>
+    /// 数据库连接信息
+    /// </summary>
+    internal class ModelData : DbContext
+    {
+        private readonly string connectionString;
+        public ModelData()
+        {
+            var builder = new MySqlConnectionStringBuilder();
+            builder.Server = "rm-cn-pe3390ehc0013txo.rwlb.rds.aliyuncs.com";
+            builder.Port = 3306;
+            builder.UserID = "root";
+            builder.Password = "ZAQ12wsx";
+            builder.Database = "ThisIsMyWar";
+            connectionString = builder.ToString();
+        }
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
--- ViewModels/IndexMainWindowViewModel.cs
+++ ViewModels/IndexMainWindowViewModel.cs
@@ -2,15 +2,18 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using ThisIsMyWar.Models;
 
 namespace ThisIsMyWar.ViewModels
 {
-
-
+    /// <summary>
+    /// 数据库操作参考
+    /// </summary>
     internal class IndexMainWindowViewModel : BindableBase
     {
         private string title;
@@ -19,32 +22,66 @@ namespace ThisIsMyWar.ViewModels
             get { return title; }
             set { SetProperty(ref title, value); }
         }
-        string connectionString = "server=rm-cn-pe3390ehc0013txo.rwlb.rds.aliyuncs.com;user id=root;password=ZAQ12wsx;persistsecurityinfo=True;database=ThisIsMyWar;charset=utf8;";
+        private ModelData dbConnection;
         public IndexMainWindowViewModel()
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            dbConnection = new ModelData();
+            Add();
+            Get();
+        }
+        //查询
+        public void Get()
+        {
+            using var connection = dbConnection.GetConnection();
+            connection.Open();
+            string ID = "2";
+            string query = $"SELECT * FROM User WHERE ID={ID}";
+            string name = "";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            using (MySqlDataReader reader = command.ExecuteReader())
             {
-                try
+                if (reader.Read())
                 {
-                    connection.Open();
-                    string query = "SELECT * FROM User WHERE ID=2;";
-                    string name="";
-                    MySqlCommand command = new MySqlCommand(query, connection);
-                    using (MySqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            name = reader.GetString("Name");
-                        }
-                    }
-                    Title = name;
-                }
-                catch (Exception ex)
-                {
-                    // 处理连接错误
-                    Console.WriteLine(ex.Message);
+                    name = reader.GetString("Name");
                 }
             }
+            Title = name;
+            connection.Close();
+        }
+        //添加
+        public void Add()
+        {
+            using var connection = dbConnection.GetConnection();
+            connection.Open();
+            string ID = "3";
+            string Name = "WZJ";
+            string query = $"INSERT INTO `thisismywar`.`user` (`ID`, `Name`) VALUES ({ID}, '{Name}')";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        //删除
+        public void Delete()
+        {
+            using var connection = dbConnection.GetConnection();
+            connection.Open();
+            string ID = "3";
+            string query = $"DELETE FROM `thisismywar`.`user` WHERE `ID` = {ID}";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        //修改
+        public void Update()
+        {
+            using var connection = dbConnection.GetConnection();
+            connection.Open();
+            string ID = "3";
+            string Name = "hexiancheng";
+            string query = $"UPDATE `thisismywar`.`user` SET `Name` = '{Name}' WHERE `ID` = {ID}";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
